c69cae71b3e1a20e3a1c412bba952e1bad6c2f58
Kuldeep Singh <ksingh51@uic.edu>
2022-11-09 21:55:39 -0600

    Radar Done

--- src/js/radar.js
+++ src/js/radar.js
@@ -1,133 +1,115 @@
-dataset = [4, 2, 5, 7, 8, 9, 2, 6, 3, 3, 10];
+let radarData = [];
+let features = [
+  "Albumin",
+  "Creatine",
+  "Hemoglboin",
+  "Weight",
+  "Systolic",
+  "DiSystolic",
+];
+//generate the data
+for (var i = 0; i < 3; i++) {
+  var point = {};
+  //each feature will be a random number from 1-9
+  features.forEach((f) => (point[f] = 1 + Math.random() * 8));
+  radarData.push(point);
+}
 
-getTheta = (dataset) => (Math.PI * 2) / dataset.length;
+var margin = {
+    top: 10,
+    bottom: 10,
+    left: 10,
+    right: 10,
+  },
+  width = parseInt(d3.select("#radar_plot").style("width")),
+  mapRatio = 0.75,
+  height = width * mapRatio;
 
-isWithin = (min, max) => (value) => value >= min && value <= max;
+let radarSvg = d3
+  .select("#radar_plot")
+  .append("svg")
+  .attr("width", width + margin.left + margin.right)
+  .attr("height", height + margin.top + margin.bottom);
 
-drawAxis = (dataset, scale) => (selection) => {
-  selection
-    .append("g")
-    .attr("class", "axis")
-    .selectAll(".tick")
-    .data(d3.range(d3.max(dataset) + 1))
-    .join("circle")
-    .attr("class", "tick")
-    .attr("r", (d, i) => scale(i))
-    .attr("fill", "lightgrey")
-    .attr("fill-opacity", 0.2)
-    .attr("stroke", "lightgrey")
-    .attr("stroke-width", 0.5);
-};
+let radialScale = d3.scaleLinear().domain([0, 10]).range([0, 160]);
+let ticks = [2, 4, 6, 8, 10];
 
-drawGuideLines = (dataset, r) => (selection) => {
-  selection
-    .selectAll(".guideline")
-    .data(dataset)
-    .join("line")
-    .attr("class", "guideline")
-    .attr("x1", 0)
-    .attr("x2", (d, i) => r * Math.sin(getTheta(dataset) * i))
-    .attr("y1", 0)
-    .attr("y2", (d, i) => r * -Math.cos(getTheta(dataset) * i))
-    .attr("stroke", "white")
-    .attr("stroke-width", 1);
-};
-
-drawLabels = (dataset, r) => (selection) => {
-  const arc = d3
-    .arc()
-    .innerRadius(r + 10)
-    .outerRadius(r + 10);
-  selection
-    .selectAll(".arc")
-    .data(dataset)
-    .join("path")
-    .attr("class", "arc")
-    .attr("id", (d, i) => `arc-${i}`)
-    .attr("fill", "red")
-    .attr("fill-opacity", 0.5)
-    .attr("d", (d, i) =>
-      arc({
-        startAngle: i * getTheta(dataset),
-        endAngle: (i + 1) * getTheta(dataset),
-      })
-    );
-  selection
-    .append("g")
-    .attr("style", `transform: rotate(-${getTheta(dataset) / 2}rad)`)
-    .selectAll("text")
-    .data(dataset)
-    .join("text")
-    .attr("font-size", 8)
-    .attr("text-anchor", "middle")
-    .attr("dy", 3)
-    .append("textPath")
-    .attr("startOffset", (d, i) =>
-      isWithin(
-        (90 * Math.PI) / 180,
-        (270 * Math.PI) / 180
-      )(i * getTheta(dataset))
-        ? "75%"
-        : "25%"
-    )
-    .attr("xlink:href", (d, i) => `#arc-${i}`)
-    .text((d, i) => `Value[${i}] ${d}`);
-};
-
-drawPolygon = (dataset, scale) => (selection) => {
-  const polygon = d3
-    .radialLine()
-    .angle((_, i) => i * getTheta(dataset) + Math.PI)
-    .curve(d3.curveCardinalClosed)
-    .radius((d, i) => scale(d));
-  selection
-    .append("path")
-    .attr("d", polygon(dataset))
-    .attr("stroke", "lightgrey")
-    .attr("stroke-width", 0.5)
-    .attr("fill", "Teal")
-    .attr("fill-opacity", 0.5)
-    .attr("transform", `rotate(180)`);
-};
-
-drawMarks = (dataset, scale) => (selection) => {
-  selection
-    .selectAll(".mark")
-    .data(dataset)
-    .enter()
+ticks.forEach((t) =>
+  radarSvg
     .append("circle")
-    .attr("class", "mark")
-    .attr("cx", (d, i) => scale(d) * Math.sin(getTheta(dataset) * i))
-    .attr("cy", (d, i) => scale(d) * -Math.cos(getTheta(dataset) * i))
-    .attr("fill", "Teal")
-    .attr("r", 2);
-};
+    .attr("cx", 250)
+    .attr("cy", 200)
+    .attr("fill", "none")
+    .attr("stroke", "gray")
+    .attr("r", radialScale(t))
+);
 
-{
-  const width = 300;
-  const height = 300;
-  //   const svg = d3.create("svg").attr("viewBox", [0, 0, width, height]);
+ticks.forEach((t) =>
+  radarSvg
+    .append("text")
+    .attr("x", 255)
+    .attr("y", 200 - radialScale(t))
+    .text(t.toString())
+);
 
-  const viz = d3
-    .select(".div2")
-    .append("svg")
-    .attr("viewBox", [0, 0, width, height])
-    .append("g")
-    .attr("transform", `translate(${width / 2}, ${height / 2})`);
+function angleToCoordinate(angle, value) {
+  let x = Math.cos(angle) * radialScale(value);
+  let y = Math.sin(angle) * radialScale(value);
+  return { x: 250 + x, y: 200 - y };
+}
 
-  const r = 80;
+for (var i = 0; i < features.length; i++) {
+  let ft_name = features[i];
+  let angle = Math.PI / 2 + (2 * Math.PI * i) / features.length;
+  let line_coordinate = angleToCoordinate(angle, 10);
+  let label_coordinate = angleToCoordinate(angle, 10.5);
 
-  const scale = d3
-    .scaleLinear()
-    .domain([0, d3.max(dataset)])
-    .range([0, r]);
+  //draw axis line
+  radarSvg
+    .append("line")
+    .attr("x1", 250)
+    .attr("y1", 200)
+    .attr("x2", line_coordinate.x)
+    .attr("y2", line_coordinate.y)
+    .attr("stroke", "black");
 
-  viz
-    .call(drawAxis(dataset, scale))
-    .call(drawGuideLines(dataset, r))
-    .call(drawLabels(dataset, r))
-    .call(drawPolygon(dataset, scale))
-    .call(drawMarks(dataset, scale));
+  //draw axis label
+  radarSvg
+    .append("text")
+    .attr("x", label_coordinate.x)
+    .attr("y", label_coordinate.y)
+    .text(ft_name);
+}
 
-  //   return svg.node();
+let line = d3
+  .line()
+  .x((d) => d.x)
+  .y((d) => d.y);
+let colors = ["darkorange", "gray", "navy"];
+
+function getPathCoordinates(data_point) {
+  let coordinates = [];
+  for (var i = 0; i < features.length; i++) {
+    let ft_name = features[i];
+    let angle = Math.PI / 2 + (2 * Math.PI * i) / features.length;
+    coordinates.push(angleToCoordinate(angle, data_point[ft_name]));
+  }
+  return coordinates;
+}
+
+for (var i = 0; i < radarData.length; i++) {
+  let d = radarData[i];
+  let color = colors[i];
+  let coordinates = getPathCoordinates(d);
+
+  //draw the path element
+  radarSvg
+    .append("path")
+    .datum(coordinates)
+    .attr("d", line)
+    .attr("stroke-width", 3)
+    .attr("stroke", color)
+    .attr("fill", color)
+    .attr("stroke-opacity", 1)
+    .attr("opacity", 0.5);
 }
