a99f2c1a49f71cce880931653bd93ae016c88d94
Aditya Ranganathan <ranga.aditya97@gmail.com>
2022-11-19 21:07:42 -0600

    layout v1.0

--- src/js/heatmap.js
+++ src/js/heatmap.js
@@ -1,11 +1,11 @@
 // set the dimensions and margins of the graph
-const margin = {top: 30, right: 30, bottom: 30, left: 30},
+var margin = {top: 30, right: 30, bottom: 30, left: 30},
   width = 450 - margin.left - margin.right,
   height = 450 - margin.top - margin.bottom;
 
 // console.log('shit')
 // append the svg object to the body of the page
-const svg = d3.select("#heat_map")
+const heat_svg = d3.select("#heat_map")
 .append("svg")
   .attr("width", width + margin.left + margin.right)
   .attr("height", height + margin.top + margin.bottom)
@@ -21,7 +21,7 @@ const x = d3.scaleBand()
   .range([ 0, width ])
   .domain(myGroups)
   .padding(0.01);
-svg.append("g")
+heat_svg.append("g")
   .attr("transform", `translate(0, ${height})`)
   .call(d3.axisBottom(x))
 
@@ -30,7 +30,7 @@ const y = d3.scaleBand()
   .range([ height, 0 ])
   .domain(myVars)
   .padding(0.01);
-svg.append("g")
+heat_svg.append("g")
   .call(d3.axisLeft(y));
 
 // Build color scale
@@ -70,7 +70,7 @@ const myColor = d3.scaleLinear()
   
   // add the squares
   console.log(data)
-  svg.selectAll()
+  heat_svg.selectAll()
     .data(data, function(d) {return d.group+':'+d.variable;})
     .enter()
     .append("rect")
